43f7df96160cb302965bacd5276fe01043cc3aa7
Nicholas <nicholasb423@gmail.com>
2024-10-13 19:45:44 -0400

    Created an underconstruction image

--- ghi/src/components/Construct.jsx
+++ ghi/src/components/Construct.jsx
@@ -6,7 +6,7 @@ function Construct(props) {
     return (
         <>
             <h1>Under construction</h1>
-            <h2><img src="/Users/nicholasbailey/Downloads/Messenger_creation_6442C09A-0F8A-48A9-B0BF-E70551428C79.jpeg" /> </h2>
+            <img src="/underconstruction.JPEG" alt="Under Construction" />
         </>
     )
 }
